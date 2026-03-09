3b1e3c4b86171956d597d18c65322bdf634ea04e
MRAyoub1 <rom20008ap@gmail.com>
2026-03-09 15:30:41 +0000

    update lesson-3

--- lesson-3/index.js
+++ lesson-3/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb+srv://databaseusername:databaseusername@cluster0.yciqo8l.mongodb.net/?appName=Cluster0")
+.then(() => {
+    console.log("connect successfly with data basa");
+}).catch(() => {
+    console.log("err: faild to connect to daba base")
+})
+
+app.use(express.json());
 
 app.get("/hello", function(req, res){
     res.send("hello");
@@ -20,6 +30,10 @@ app.get("/find/:number1/:number2", (req ,res) => {
     const num2 = req.params.number2
     res.send(`the numbers are :${num1}/${num2}`);
 })
+
+app.get("/find2", (req ,res) => {
+    console.log(req.body)
+});
 
 app.listen(3000, function(){
     console.log("I am listening in port 3000")
